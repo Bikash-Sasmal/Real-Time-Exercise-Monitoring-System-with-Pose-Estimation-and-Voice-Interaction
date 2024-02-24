762e296a8ad424cbccadac16a404c3a6ba9415ba
Bikash-Sasmal <bikash.sasmal.mca.2022@nist.edu>
2024-02-24 11:27:47 +0530

    add final year project

---
+++ left_hand_dumble_lifting.py
@@ -0,0 +1,70 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+from calculate_angle import calculate_angle
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+cap = cv2.VideoCapture(0)
+counter = 0
+stage = "up"
+
+def perform_pose_left(hand = "left"):
+    cap = cv2.VideoCapture(0)
+    counter = 0
+    stage = None
+
+
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            # ret, frame = cap.read()
+            # image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            ret, frame = cap.read()
+            if not ret:
+             print("Error: Failed to read frame from video capture")
+    # Add appropriate error handling or exit the program
+            else:
+    # Continue processing the frame
+             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+            result = pose.process(image)
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            try:
+                landmarks = result.pose_landmarks.landmark
+                shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            
+               
+                angle = calculate_angle(shoulder, elbow, wrist)
+                
+                cv2.putText(image, str(angle), tuple(np.multiply(elbow, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+                if angle > 160:
+                    stage = " down"
+                    # speak("Down")
+                elif angle < 30 and stage == " down":
+                    stage = " up"
+                    counter += 1
+                    print(counter)
+                    # speak("Up")
+            except:
+                pass
+
+            cv2.rectangle(image, (0, 0), (255, 73), (245, 117, 16), -1)
+            cv2.putText(image, 'REPS', (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(image, 'STAGE', (65, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage, (60, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            mp_drawing.draw_landmarks(image, result.pose_landmarks, mp_pose.POSE_CONNECTIONS, mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2), mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2))
+
+            cv2.imshow("Mediapipe Feed", image)
+
+            if cv2.waitKey(10) & 0XFF == ord('q'):
+                break
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,63 @@
+import pyttsx3
+import speech_recognition as sr
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+# print(voices)
+engine.setProperty('rate',170)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        r.energy_threshold = 300
+        # r.adjust_for_ambient_noise(source,1.2)
+        audio = r.listen(source,0,4)
+
+    try:
+        print("Recognising...")
+        query = r.recognize_google(audio,language='en-in')
+        print(f"You said: {query}\n")
+    except Exception as e:
+        print(f"Say that again")
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    while True:
+        query = takeCommand().lower()
+        if "start training" in query or "start raining" in query:
+            speak("ok,sir")
+            speak("which training you would like to do")
+        
+    
+            while True:
+                query = takeCommand().lower()
+                if "go to sleep" in query:
+                    speak("ok sir , You can call me anytime")
+                    break
+                elif "left hand" in query:
+                  speak("Let's start your left side hand doumble lifting,sir")
+                  from left_hand_dumble_lifting import  perform_pose_left
+                  perform_pose_left(hand = "left")
+
+                elif "right hand" in query:
+                  from right_hand_dumble_lifting import  perform_pose_right
+                  speak("Let's start your right side hand doumble lifting,sir")
+                  perform_pose_right(hand = "right", side = "right")
+        
+                elif "exit" in query:
+                  speak("exit in this programm, sir")
+                  exit()
+
+
+
+
+
